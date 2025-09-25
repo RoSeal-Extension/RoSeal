54974b3585b541f676513514d5f38357e69e8d19
Julli4n <11952329+Julli4n@users.noreply.github.com>
2025-09-25 09:08:06 -0700

    refactor(user): extract collectible items for RAP value into its own component

--- src/ts/components/users/userProfile/UserRAPHeader.tsx
+++ src/ts/components/users/userProfile/UserRAPHeader.tsx
@@ -6,11 +6,8 @@ import { getMessage } from "src/ts/helpers/i18n/getMessage";
 import { useState } from "preact/hooks";
 import SimpleModal from "../../core/modal/SimpleModal";
 import useProfileData from "../../hooks/useProfileData";
-import { getAvatarAssetLink } from "src/ts/utils/links";
-import RobuxView from "../../core/RobuxView";
-import Thumbnail from "../../core/Thumbnail";
-import Icon from "../../core/Icon";
 import { SEAL_EMOJI_COMPONENT } from "src/ts/constants/preact";
+import UserRAPItem from "./UserRAPItem";
 
 export type UserRAPHeaderProps = {
 	userId: number;
@@ -33,32 +30,27 @@ export default function UserRAPHeader({ userId }: UserRAPHeaderProps) {
 		return rap;
 	}, [alllCollectibles]);
 
-	if (userRAP === null || userRAP === undefined) return null;
-
-	const countDisplay = abbreviateNumber(userRAP);
-	if (!countDisplay) return null;
+	const countDisplay =
+		userRAP !== undefined && userRAP !== null ? abbreviateNumber(userRAP) : "...";
+	const fullCount = userRAP ? asLocaleString(userRAP) : "...";
 
 	return (
 		<>
 			<SocialHeader
 				title={getMessage("user.header.social.rap")}
 				alt={getMessage("user.header.social.rap.alt", {
-					count: asLocaleString(userRAP),
+					count: fullCount,
 				})}
 				value={countDisplay}
 				className="roseal-rap-value"
-				onClick={
-					alllCollectibles?.length === 0
-						? undefined
-						: () => setShowCollectiblesModal(true)
-				}
+				onClick={alllCollectibles ? () => setShowCollectiblesModal(true) : undefined}
 			/>
 			<SimpleModal
 				show={showCollectiblesModal}
 				size="lg"
 				title={getMessage("user.header.social.rap.modal.title", {
 					displayName: profileData?.names.combinedName ?? "",
-					count: asLocaleString(alllCollectibles?.length ?? 0),
+					count: fullCount,
 					countNum: alllCollectibles?.length ?? 0,
 					sealEmoji: SEAL_EMOJI_COMPONENT,
 				})}
@@ -67,50 +59,7 @@ export default function UserRAPHeader({ userId }: UserRAPHeaderProps) {
 			>
 				<ul className="hlist item-cards roseal-scrollbar">
 					{alllCollectibles?.map((item) => (
-						<li className="list-item item-card">
-							<div className="item-card-container">
-								<a href={getAvatarAssetLink(item.assetId, item.name)}>
-									<div className="item-card-link">
-										<div className="item-card-thumb-container">
-											<Thumbnail
-												request={{
-													type: "Asset",
-													targetId: item.assetId,
-													size: "420x420",
-												}}
-											/>
-											<div className="limited-icon-container">
-												<Icon name="shop-limited" />
-												{item.serialNumber !== undefined &&
-													item.serialNumber !== null && (
-														<span className="font-caption-header text-subheader limited-number">
-															{getMessage(
-																"user.header.social.rap.modal.body.item.serialNumber",
-																{
-																	serialNumber: asLocaleString(
-																		item.serialNumber,
-																	),
-																},
-															)}
-														</span>
-													)}
-											</div>
-										</div>
-									</div>
-									<div className="item-card-caption">
-										<div className="item-card-name-link">
-											<div className="item-card-name" title={item.name}>
-												{item.name}
-											</div>
-											<RobuxView
-												priceInRobux={item.recentAveragePrice}
-												containerClassName="text-overflow item-card-price"
-											/>
-										</div>
-									</div>
-								</a>
-							</div>
-						</li>
+						<UserRAPItem item={item} />
 					))}
 				</ul>
 			</SimpleModal>
---
+++ src/ts/components/users/userProfile/UserRAPItem.tsx
@@ -0,0 +1,57 @@
+import { getAvatarAssetLink } from "src/ts/utils/links";
+import Thumbnail from "../../core/Thumbnail";
+import type { ListedUserCollectibleAsset } from "src/ts/helpers/requests/services/inventory";
+import Icon from "../../core/Icon";
+import { getMessage } from "src/ts/helpers/i18n/getMessage";
+import { asLocaleString } from "src/ts/helpers/i18n/intlFormats";
+import RobuxView from "../../core/RobuxView";
+
+export type UserRAPItemProps = {
+	item: ListedUserCollectibleAsset;
+};
+
+export default function UserRAPItem({ item }: UserRAPItemProps) {
+	return (
+		<li className="list-item item-card">
+			<div className="item-card-container">
+				<a href={getAvatarAssetLink(item.assetId, item.name)}>
+					<div className="item-card-link">
+						<div className="item-card-thumb-container">
+							<Thumbnail
+								request={{
+									type: "Asset",
+									targetId: item.assetId,
+									size: "420x420",
+								}}
+							/>
+							<div className="limited-icon-container">
+								<Icon name="shop-limited" />
+								{item.serialNumber !== undefined && item.serialNumber !== null && (
+									<span className="font-caption-header text-subheader limited-number">
+										{getMessage(
+											"user.header.social.rap.modal.body.item.serialNumber",
+											{
+												serialNumber: asLocaleString(item.serialNumber),
+											},
+										)}
+									</span>
+								)}
+							</div>
+						</div>
+					</div>
+					<div className="item-card-caption">
+						<div className="item-card-name-link">
+							<div className="item-card-name" title={item.name}>
+								{item.name}
+							</div>
+							<RobuxView
+								priceInRobux={item.recentAveragePrice}
+								containerClassName="text-overflow item-card-price"
+							/>
+						</div>
+					</div>
+				</a>
+			</div>
+		</li>
+	);
+}
